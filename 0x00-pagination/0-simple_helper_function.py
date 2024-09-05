217a077f425172116880049666c70cc7d57ca2d7
Haweanah <ahauwa48@yahoo.com>
2024-09-05 14:41:42 -0700

    Pagination

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -15,11 +15,11 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
 
     Returns:
         tuple: start and end indexes of current page.
-        """
-        # calculate start index by subtracting 1 from the current page number
-        # and then multiplying by the page size
-        start_index = (page - 1) * page_size
-        # calculate end index by adding the start index to the page size
-        end_index = start_index + page_size
-        # return the start and end indexes as a tuple
-        return (start_index, end_index)
+    """
+    # calculate start index by subtracting 1 from the current page number
+    # and then multiplying by the page size
+    start_index = (page - 1) * page_size
+    # calculate end index by adding the start index to the page size
+    end_index = start_index + page_size
+    # return the start and end indexes as a tuple
+    return (start_index, end_index)
